9096caeea13a147da2370df807c43213f4110532
MathieuLemoyne <159171247+MathieuLemoyne@users.noreply.github.com>
2025-04-03 17:11:00 -0400

    fin recit2

---
+++ tp2/src/components/HighScoreTable.vue
@@ -0,0 +1,56 @@
+<script setup lang="ts">
+import { computed } from "vue";
+
+const props = defineProps<{
+  winningPlayers: Array<{
+    name: string;
+    credits: number;
+  }>;
+}>();
+
+const sortedPlayers = computed(() => {
+  return props.winningPlayers.sort((a, b) => b.credits - a.credits);
+});
+</script>
+
+<template>
+  <div class="winning-players">
+    <h2>Liste des Joueurs Gagnants</h2>
+
+    <div
+      v-for="(player, index) in sortedPlayers"
+      :key="index"
+      class="player-card card"
+    >
+      <ul>
+        <li><strong>Nom :</strong> {{ player.name }}</li>
+        <li><strong>Crédits Galactiques :</strong> {{ player.credits }}</li>
+      </ul>
+    </div>
+  </div>
+</template>
+<style scoped>
+.card {
+  padding: 1rem;
+  border-radius: 8px;
+  background-color: #1e293b;
+  color: white;
+  width: 300px;
+  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.3);
+}
+
+h2 {
+  margin-bottom: 1rem;
+  font-size: 1.25rem;
+  text-align: center;
+}
+
+ul {
+  list-style: none;
+  padding: 0;
+}
+
+li {
+  margin-bottom: 0.5rem;
+}
+</style>
--- tp2/src/views/HomeView.vue
+++ tp2/src/views/HomeView.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import CharacterStatus from "@/components/CharacterStatus.vue";
 import TitleScreen from "../components/TitleScreen.vue";
+import HighScoreTable from "@/components/HighScoreTable.vue";
 </script>
 
 <template>
@@ -14,6 +15,13 @@ import TitleScreen from "../components/TitleScreen.vue";
       :credits="0"
       shipName="riendutout"
       :health="100"
+    />
+    <HighScoreTable
+      :winningPlayers="[
+        { name: 'Player 1', credits: 5000 },
+        { name: 'Player 2', credits: 3000 },
+        { name: 'Player 3', credits: 7000 },
+      ]"
     />
   </main>
 </template>
