a91b2a84a1e889e0c209a6c6ffad3102350a4191
MathieuLemoyne <159171247+MathieuLemoyne@users.noreply.github.com>
2025-04-03 16:22:07 -0400

    image background

--- tp2/src/components/TitleScreen.vue
+++ tp2/src/components/TitleScreen.vue
@@ -14,13 +14,31 @@ export default defineComponent({
 </script>
 
 <template>
-  <div class="container">
-    <img src="./assets/background01.jpg" />
-    <div class="centered">
+  <div class="container-fluid position-relative">
+    <!-- Background image -->
+    <img
+      src="../assets/background01.jpg"
+      class="img-fluid position-absolute top-0 start-0 w-100"
+      alt="background"
+    />
+
+    <!-- Centered content -->
+    <div class="centered position-relative z-index-10 text-center text-white">
       <h1 class="display-3 fw-bold">Goon's Adventure</h1>
-      <button class="btn btn-primary" @click="startGame">START</button>
-      <button class="btn btn-secondary" @click="showCredits">CREDITS</button>
-      <button class="btn btn-danger" @click="exitGame">EXIT</button>
+      <button class="btn btn-primary mb-3" @click="startGame">START</button>
+      <button class="btn btn-secondary mb-3" @click="showCredits">
+        CREDITS
+      </button>
+      <button class="btn btn-danger mb-3" @click="exitGame">EXIT</button>
     </div>
   </div>
 </template>
+
+<style scoped>
+.centered {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+}
+</style>
