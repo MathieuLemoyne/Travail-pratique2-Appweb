0bf1b8d3efef8ca6ab3e6434bc557daa09320c36
MathieuLemoyne <159171247+MathieuLemoyne@users.noreply.github.com>
2025-04-03 16:38:50 -0400

    fix2

--- tp2/src/App.vue
+++ tp2/src/App.vue
@@ -1,7 +1,5 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from "vue-router";
-import HelloWorld from "./components/HelloWorld.vue";
-import TitleScreen from "./components/TitleScreen.vue";
 </script>
 
 <template>
