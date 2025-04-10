5cf1fe7305b524efa94555db81d32cd1904dfd05
MathieuLemoyne <159171247+MathieuLemoyne@users.noreply.github.com>
2025-04-10 16:48:12 -0400

    fix de recit8

--- tp2/src/components/MissionStatus.vue
+++ tp2/src/components/MissionStatus.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 import { ref } from "vue";
-import { missionCourante, totalMissions } from "@/gameState";
+import { missionCourante, totalMissions } from "../services/gameState.ts";
 </script>
 <template>
   <div class="mission-en-cours">
--- tp2/src/views/GameView.vue
+++ tp2/src/views/GameView.vue
@@ -1,4 +1,7 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import MissionStatus from "@/components/MissionStatus.vue";
+</script>
 <template>
+  <MissionStatus />
   <h1>LE JEU EST COMMENCÉ RAHHHHH</h1>
 </template>
